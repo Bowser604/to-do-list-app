935864a016bb483bcbbc741098cfa1be5ff875e2
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-12-07 19:38:24 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,49 @@
-let li = $('<li></li>');
-let inputValue = $('#input').val();
+function newItem() {
+  
+  // 1. Adding a new item to the list of items 
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
   li.append(inputValue);
 
-if (inputValue === '') {
-  alert("You must write something!");
-} else {
-  $('#list').append(li);
-}
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    let list = $('#list');
+    list.append(li);
+    $('#input').val(""); // Reset input field
+  }
 
-// Crossing out items
-function crossOut() {
-	li.toggleClass("strike");
-}
+  // 2. Crossing out items from the list
+  function crossOut() {
+    li.toggleClass("strike");
+  }
 
-li.on("dblclick", function crossOut() {
-	li.toggleClass("strike");
-}); 
+  li.on("dblclick", crossOut)
 
-// Adding a delete button
-let crossOutButton = $('<crossOutButton></crossOutButton>');
+  // 3.(a) Adding a 'delete' button "X"
+  let crossOutButton = $('<button></button>');
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
 
-//  crossOutButton.on("click", deleteListItem);
-//  function deleteListItem(){
-// 	li.addClass("delete")
-// 	}
+  crossOutButton.on("click", deleteListItem);
+
+  // 3.(b) Adding class delete(display: none) from the css
+  function deleteListItem() {
+    li.addClass("delete")
+  }
+
+  // 4. Recording the item
   $('#list').sortable();
+}
+
+function newItemOnEnter(event) {
+  if (event.code == "Enter") {
+    event.preventDefault(); // Dont submit form
+    newItem();
+  }
+}
+
+// Attach event listener for Enter key press
+$('#input').on("keydown", newItemOnEnter);
 
  
